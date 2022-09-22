9b0a9b8f7e06cad1d8aa781df9a98c7a56ccaa43
HaudegenHH <einsundeins79@gmx.de>
2022-09-22 04:05:01 +0200

    changed server.js

--- server/server.js
+++ server/server.js
@@ -1,73 +1,37 @@
-<<<<<<< HEAD
-import express from 'express'
-import mongoose from 'mongoose'
-import connect from './database/mongodb.js'
-import Transaction from './models/Transaction.js'
-import transactionRoutes from './routes/transactions.js'
+import express from 'express';
+import mongoose from 'mongoose';
+import connect from './database/mongodb.js';
+import Transaction from './models/Transaction.js';
+import transactionRoutes from './routes/transactions.js';
 
-import cors from 'cors'
+import cors from 'cors';
 
 // JSON and URL-encoded parser
 // top-level-middleware to parse the body of all incoming requests
-import bodyParser from 'body-parser'
+import bodyParser from 'body-parser';
 
-import * as dotenv from 'dotenv'
-dotenv.config()
+import * as dotenv from 'dotenv';
+dotenv.config();
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
-
-
-const app = express()
-
-
+const app = express();
 
 // ---------- middleware ------------ //
-app.use(cors())
+app.use(cors());
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 // transaction Routes
-app.use('/transactions', transactionRoutes)
+app.use('/transactions', transactionRoutes);
 
 // ----------------------------------- //
 
 // since the application cant run without a DB connection..
 // first wait for the connection to MongoDB, (only) then start the express server
-await connect()
+await connect();
 
 app.listen(PORT, () => {
-  console.log(`Server is running at http://localhost:${PORT}`)
-})
-=======
-import express from 'express'
-import mongoose from 'mongoose'
-import cors from 'cors'
-import * as dotenv from 'dotenv'
-
-dotenv.config()
-
-const PORT = process.env.PORT || 4000
-const DB_URI = process.env.MONGODB_URI
-
-
-const app = express()
-app.use(cors())
-
-
-// since the application cant run without a DB connection..
-// first wait for the connection to MongoDB, (only) then start the express server
-await mongoose.connect(DB_URI)
-console.log('Successfully connected to MongoDB')
-
-
-app.get('/', (req, res) => {
-  res.send('Hello world')
-})
-
-
-app.listen(PORT, () => {
-  console.log(`Server is running at http://localhost:${PORT}`)
-})
->>>>>>> 67beb04422e6cb826e956bec94565180cdda1c2a
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
